2660e64c2682e0417d71fab567f0709e547cb7cb
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-05-28 20:43:32 +0100

    Add Comments Popup and test files

--- src/modules/moviesCount.js
+++ src/modules/moviesCount.js
@@ -1,12 +1,1 @@
-import getData from './getdata.js';
-
-const countedMovies = document.querySelector('.num-of-movies');
-
-const counterFunction = async () => {
-  const moviesNumber = await getData();
-  const total = moviesNumber.length;
-  countedMovies.textContent = `Movie Shows(${total})`;
-  return total;
-};
-
-counterFunction();
+// const counterMovies = document.querySelector('.')
